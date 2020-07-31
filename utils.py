016a9549fc093326b51b85d7c6edd0ee5c95231b
Rasul Kireev <me@rasulkireev.com>
2020-07-31 11:26:17 -0400

    instructions function added

---
+++ utils.py
@@ -0,0 +1,8 @@
+def instructions():
+    print()
+    print("Let's Play!!!")
+    print("Which version of Rock-Paper-Scissors?")
+    print("Enter 1 to play Rock-Paper-Scissors")
+    print("Enter 2 to play Rock-Paper-Scissors-Lizard-Spock")
+    print("Enter 3 to quit")
+    print()
