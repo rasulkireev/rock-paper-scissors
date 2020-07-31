f3a30e024a6741718c38db830ef4b6d6901e84fe
Rasul Kireev <me@rasulkireev.com>
2020-07-31 11:24:42 -0400

    Project initiated.

---
+++ main.py
@@ -0,0 +1,6 @@
+# The mapping between moves and numbers
+game_map = {0:"rock", 1:"paper", 2:"scissors"}
+ 
+# Win-lose matrix for traditional game
+rps_table = [[-1, 1, 0], [1, -1, 2], [0, 2, -1]]
+
